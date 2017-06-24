7cc0825dd97ebb2333ccee13e8a122a51a438879
oncemore <oncemoreforyou@outlook.com>
2017-06-24 03:07:16 +0200

    Added New ViewController
    Added ReferralViewController

---
+++ beautyios/ViewControllers/Main/ProfileSurveyController.swift
@@ -0,0 +1,22 @@
+//
+//  ProfileSurveyController.swift
+//  beautyios
+//
+//  Created by Michael on 6/24/17.
+//  Copyright © 2017 Michael. All rights reserved.
+//
+
+import Foundation
+
+
+class ProfileSurveyController: UIViewController{
+    
+    override func viewDidLoad() {
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+}
---
+++ beautyios/ViewControllers/Main/ReferralViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ReferralViewController.swift
+//  beautyios
+//
+//  Created by Michael on 6/24/17.
+//  Copyright © 2017 Michael. All rights reserved.
+//
+
+import Foundation
+
+class ReferralViewController: ViewController{
+    
+    
+    override func viewDidLoad() {
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+}
